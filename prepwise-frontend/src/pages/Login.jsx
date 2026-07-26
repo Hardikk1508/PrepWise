ad6ae61a36bc3fe472bb35c6fd246c7af4d241b4
Hardik Bhardwaj <hardik.bhardwaj500@gmail.com>
2026-07-26 15:10:39 +0530

    Fix API_URL declaration

--- prepwise-frontend/src/pages/Login.jsx
+++ prepwise-frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const colors = {
   warning: "#ca8a04",
   danger: "#dc2626",
 };
-
+const API_URL = import.meta.env.VITE_API_URL;
 export default function Login() {
   const [isLogin, setIsLogin] = useState(true);
   const navigate = useNavigate();
